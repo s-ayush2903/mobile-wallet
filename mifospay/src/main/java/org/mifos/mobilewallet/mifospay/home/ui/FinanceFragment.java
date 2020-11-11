2366493234af67af3582ce0ec064165a4a53bff4
s-ayush2903 <ayush_1901cb14@iitp.ac.in>
2020-11-12 00:09:31 +0530

    Forced failure in mifospay module too

--- mifospay/src/main/java/org/mifos/mobilewallet/mifospay/home/ui/FinanceFragment.java
+++ mifospay/src/main/java/org/mifos/mobilewallet/mifospay/home/ui/FinanceFragment.java
@@ -7,7 +7,7 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import org.mifos.mobilewallet.core.data.fineract.api.ApiEndPoints;
 import org.mifos.mobilewallet.mifospay.R;
 import org.mifos.mobilewallet.mifospay.base.BaseFragment;
 import org.mifos.mobilewallet.mifospay.bank.ui.AccountsFragment;
